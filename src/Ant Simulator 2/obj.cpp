5d578826b6ecc1a0e783e048eea1c4e7c4a7ddce
AndrewMcCrary <ajmccrary03@gmail.com>
2021-06-22 23:36:42 -0400

    obj

--- src/Ant Simulator 2/obj.cpp
+++ src/Ant Simulator 2/obj.cpp
@@ -1,1 +1,41 @@
 #include "obj.h"
+
+obj::obj() {
+
+}
+
+obj::obj(float _x, float _y, sf::ConvexShape _asset) {
+
+}
+
+sf::ConvexShape obj::getAsset() {
+	return this->asset;
+}
+
+void obj::setAsset(sf::ConvexShape _asset) {
+	this->asset = _asset;
+}
+
+float obj::getRotation() {
+	return this->getAsset().getRotation();
+}
+
+void obj::setRotation(float _a) {
+	this->getAsset().setRotation(_a);
+}
+
+float obj::getX() {
+	return this->getAsset().getPosition().x;
+}
+
+void obj::setX(float _x) {
+	this->getAsset().setPosition(_x, this->getY());
+}
+
+float obj::getY() {
+	return this->getAsset().getPosition().y;
+}
+
+void obj::setY(float _y) {
+	this->getAsset().setPosition(this->getX(), _y);
+}
--- src/Ant Simulator 2/obj.h
+++ src/Ant Simulator 2/obj.h
@@ -1,3 +1,30 @@
-#pragma once
-class obj {};
+#ifndef OBJ_H
+#define OBJ_H
 
+#include <SFML/Graphics.hpp>
+
+class obj {
+	
+private:
+	sf::ConvexShape asset;
+
+protected:
+	sf::ConvexShape getAsset();
+	void setAsset(sf::ConvexShape _asset);
+
+public:
+	obj();
+	obj(float _x, float _y, sf::ConvexShape _asset);
+
+
+	float getRotation();
+	void setRotation(float _a);
+
+	float getX();
+	void setX(float _x);
+
+	float getY();
+	void setY(float _y);
+};
+
+#endif // !OBJ_H
