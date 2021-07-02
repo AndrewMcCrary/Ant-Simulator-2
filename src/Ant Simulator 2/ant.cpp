4e614179e1c070b12637b83081f2be8fb78632d1
AndrewMcCrary <ajmccrary03@gmail.com>
2021-07-02 00:08:46 -0400

    Increased Resolution and number of ants

--- src/Ant Simulator 2/ant.cpp
+++ src/Ant Simulator 2/ant.cpp
@@ -2,12 +2,13 @@
 
 ant::ant(float _x, float _y, float _angle) {
     srand((unsigned)time(NULL));
+    this->_turnRate = 0;
 	sf::ConvexShape* temp = new sf::ConvexShape();
 
     temp->setPointCount(3);
-    temp->setPoint(0, sf::Vector2f(0.f, -10.f));
-    temp->setPoint(1, sf::Vector2f(7.5f, 10.f));
-    temp->setPoint(2, sf::Vector2f(-7.5f, 10.f));
+    temp->setPoint(0, sf::Vector2f(0.f, -5.f));
+    temp->setPoint(1, sf::Vector2f(3.75f, 5.f));
+    temp->setPoint(2, sf::Vector2f(-3.75f, 5.f));
     temp->setFillColor(ANT_COLOR);
 
     temp->setPosition(_x, _y);
@@ -19,9 +20,9 @@ ant::ant(float _x, float _y, float _angle) {
 }
 
 void ant::tick(float _delta) {
+    this->wander(ANT_WANDER_COEFF, _delta);
     this->getAsset()->move(cosf((90.f - this->getRotation()) * M_PI / 180.f) * this->getSpeed() * _delta,
                           -sinf((90.f - this->getRotation()) * M_PI / 180.f) * this->getSpeed() * _delta);
-    this->wander(ANT_WANDER_COEFF, _delta);
 }
 
 void ant::setRotation(float _a, float _delta) {
@@ -68,7 +69,7 @@ void ant::wander(float _coeff, float _delta) {
             this->_turnRate = pow(this->_turnRate, 1.1);
         } else if (this->_turnRate < -1) {
             this->_turnRate = -1;
-            this->_turnRate = -pow(abs(this->_turnRate), 1.1);
+            this->_turnRate = -pow(abs(this->_turnRate), 1.3);
         }
 
         this->setRotation(this->getRotation() + (this->_turnRate * MAX_TURN_PER_TICK));
